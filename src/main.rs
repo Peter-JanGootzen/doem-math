6dc57773d0259990812fefb5a3fab0c3fdeda3d5
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-11-18 00:16:10 +0100

    First steps towards matrices

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 mod vector;
-use vector::Vector;
-use generic_array::typenum::{ U2, U3 };
+mod matrix;
+use matrix::Matrix;
+use vector::Vector2;
+use generic_array::typenum::{ U1, U2, U3 };
 
 fn main() {
-    let mut v1 = Vector::<U3>::default();
-    v1.data[0] = 1.0;
-    v1.data[1] = 2.0;
-    v1.data[2] = 3.0;
-    let mut v2 = Vector::<U3>::default();
-    v2.data[0] = 1.0;
-    v2.data[1] = 2.0;
-    v2.data[2] = 3.0;
+    let mut v1 = Matrix::<U3, U1>::default();
+    let mut v2 = Vector2::default();
+    v2.data[0][0] = 0.0;
+    v2.data[1][0] = 1.0;
+    let mut v3 = Vector2::default();
+    v2.data[0][0] = 1.0;
+    v2.data[1][0] = 2.0;
 
-    println!("{:?}", &v1 + &v2);
-
-    let mut v3 = Vector::<U2>::default();
-    v3.data[0] = 4.0;
-    v3.data[1] = 3.0;
-    println!("{:?}", v3.norm());
+    println!("{:?}", &v2 + &v3);
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,63 +1,66 @@
 use std::ops;
 use generic_array::GenericArray;
 use generic_array::ArrayLength;
+use generic_array::typenum::{ U1, U2, U3, U4 };
+use crate::matrix::Matrix;
 
-#[derive(Debug)]
-pub struct Vector<N: ArrayLength<f64>> {
-    pub data: GenericArray<f64, N>
-}
+pub type VectorM<M> = Matrix<M, U1>;
+pub type Vector2 = VectorM<U2>;
+pub type Vector3 = VectorM<U3>;
+pub type Vector4 = VectorM<U4>;
 
-impl<N: ArrayLength<f64>> Default for Vector<N> {
-    fn default() -> Vector<N> {
-        Vector {
-            data: GenericArray::<f64,N>::default()
-        }
-    }
-}
+//impl<N: ArrayLength<f64>> Default for VectorN<N> {
+//    fn default() -> VectorN<N> {
+//        VectorN {
+//            data: GenericArray::<ArrayLength<f64>,N>::default()
+//        }
+//    }
+//}
 
-impl<N: ArrayLength<f64>> Vector<N> {
-    pub fn norm(&self) -> Vector<N> {
-        self * (1.0 / self.length())
-    }
-    pub fn length(&self) -> f64 {
-        let mut total = 0.0;
-        for i in 0..self.data.len() {
-            total += self.data[i].powi(2);
-        }
-        total.sqrt()
-    }
-}
+//impl<N: ArrayLength<f64>> VectorN<N> {
+//    pub fn norm(&self) -> VectorN<N> {
+//        self * (1.0 / self.length())
+//    }
+//    pub fn length(&self) -> f64 {
+//        let mut total = 0.0;
+//        for i in 0..self.data.len() {
+//            total += self.data[i].powi(2);
+//        }
+//        total.sqrt()
+//    }
+//}
 
-impl<N: ArrayLength<f64>> ops::Add<&Vector<N>> for &Vector<N> {
-    type Output = Vector<N>;
+impl<M: ArrayLength<GenericArray<f64, U1>>> ops::Add<&VectorM<M>> for &VectorM<M> {
+    type Output = VectorM<M>;
 
-    fn add(self, other: &Vector<N>) -> Vector<N> {
-        let mut out = Vector::<N>::default();
-        for i in 0..self.data.len() {
-            out.data[i] = self.data[i] + other.data[i];
-        }
-        out
-    }
-}
-impl<N: ArrayLength<f64>> ops::Sub<&Vector<N>> for &Vector<N> {
-    type Output = Vector<N>;
-
-    fn sub(self, other: &Vector<N>) -> Vector<N> {
-        let mut out = Vector::<N>::default();
-        for i in 0..self.data.len() {
-            out.data[i] = self.data[i] - other.data[i];
+    fn add(self, other: &VectorM<M>) -> VectorM<M> {
+        let mut out = VectorM::<M>::default();
+        for row in 0..self.data.len() {
+            out.data[row][0] = self.data[row][0] + other.data[row][0];
         }
         out
     }
 }
-impl<N: ArrayLength<f64>> ops::Mul<f64> for &Vector<N> {
-    type Output = Vector<N>;
+//impl<N: ArrayLength<f64>> ops::Sub<&Vector<N>> for &Vector<N> {
+//    type Output = Vector<N>;
+//
+//    fn sub(self, other: &Vector<N>) -> Vector<N> {
+//        let mut out = Vector::<N>::default();
+//        for i in 0..self.data.len() {
+//            out.data[i] = self.data[i] - other.data[i];
+//        }
+//        out
+//    }
+//}
+//impl<N: ArrayLength<f64>> ops::Mul<f64> for &Vector<N> {
+//    type Output = Vector<N>;
+//
+//    fn mul(self, scalar: f64) -> Vector<N> {
+//        let mut out = Vector::<N>::default();
+//        for i in 0..self.data.len() {
+//            out.data[i] = self.data[i] * scalar;
+//        }
+//        out
+//    }
+//}
 
-    fn mul(self, scalar: f64) -> Vector<N> {
-        let mut out = Vector::<N>::default();
-        for i in 0..self.data.len() {
-            out.data[i] = self.data[i] * scalar;
-        }
-        out
-    }
-}
